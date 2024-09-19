49664587d2428cdda5e312a91f51df569e60480b
jehadkalifah <39997755+jehadkalifah@users.noreply.github.com>
2024-09-19 16:51:25 +0300

    Update test_APIfunctional.py

--- APITests/test_APIfunctional.py
+++ APITests/test_APIfunctional.py
@@ -1,7 +1,7 @@
 import pytest, time
 from utils import postData, getData
 
-baseUrl = "http://192.168.1.18:5000/api"
+baseUrl = "http://192.168.100.101:5000/api"
 
 testdata = [
     (["admin@admin", "1234"], 200, 422),
